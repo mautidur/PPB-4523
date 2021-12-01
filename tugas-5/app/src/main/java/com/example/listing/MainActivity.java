9b42722e9f5d61a7cfd4611aef7ee5f4a5c48fd9
mautidur <ridwanbramasta@gmail.com>
2021-12-01 15:45:01 +0700

    update

--- tugas-5/app/src/main/java/com/example/listing/MainActivity.java
+++ tugas-5/app/src/main/java/com/example/listing/MainActivity.java
@@ -55,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 bd.deleterecord(Integer.parseInt(nomor));
-                                listviewku.remove(position);
+                                bd.deleterecord(position);
                                 listView.invalidateViews();
                             }
                         })
